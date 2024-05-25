225086faa9378e658ce055dace6edd612384385a
allworldg <944937881@qq.com>
2024-05-25 09:50:58 +0800

    extract clock title in ClockView component

--- App.tsx
+++ App.tsx
@@ -1,50 +1,39 @@
-import React, { RefObject, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import Constants from "expo-constants";
-import {
-  Pressable,
-  Text,
-  View,
-} from "react-native";
+import { Pressable, Text, View } from "react-native";
 import InputItem from "@/components/InputItem";
 import { AntDesign } from "@expo/vector-icons";
 import { MaterialCommunityIcons, FontAwesome } from "@expo/vector-icons";
 import { STATUS } from "@/constants";
+import ClockView from "@/components/ClockView";
 const STATUSBARHEIGHT = Constants.statusBarHeight;
 
 export default function App() {
-  const [minutes, setMinutes] = useState<number>(0);
-  const [seconds, setSeconds] = useState<number>(0);
+  const [remainSeconds, setRemainSeconds] = useState<number>(0);
   const [tomatoes, setTomatoes] = useState<string>("0");
   const [rests, setRests] = useState<string>("0");
   const [times, setTimes] = useState<string>("0");
 
   const timeId = useRef<any>(0);
   const [status, setStatus] = useState<Number>(STATUS.STOP);
-  const formatTime = (time: number): string => {
-    time = Math.trunc(time);
-    return time >= 10 ? time + "" : "0" + time;
-  };
 
   const start = (): void => {
     setStatus(STATUS.WORKING);
     const now = new Date();
     const targetTime = now.getTime() + 60 * Number(tomatoes) * 1000;
     timeId.current = setTimeout(() => {
-      const remainTime = (targetTime - Date.now()) / 1000;
-      setMinutes(remainTime / 60);
-      setSeconds(remainTime % 60);
+      setRemainSeconds((targetTime - Date.now()) / 1000);
       countDown(targetTime);
     }, 10);
   };
   const countDown = (targetTime: number): void => {
-    const remainTime = (targetTime - Date.now()) / 1000;
+    const remainTime = targetTime - Date.now();
     if (remainTime <= 0) {
       stop();
       return;
     }
-    setMinutes(remainTime / 60);
-    setSeconds(remainTime % 60);
+    setRemainSeconds(remainTime / 1000);
     timeId.current = setTimeout(() => {
       countDown(targetTime);
     }, 100);
@@ -52,29 +41,13 @@ export default function App() {
   const stop = (): void => {
     clearTimeout(timeId.current);
     timeId.current = 0;
+    setRemainSeconds(0);
     setStatus(STATUS.STOP);
-    setMinutes(0);
-    setSeconds(0);
   };
 
   return (
     <View style={{ marginTop: STATUSBARHEIGHT, display: "flex", flex: 1 }}>
-      <View
-        style={{
-          display: "flex",
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 70,
-          }}
-        >
-          {formatTime(minutes) + ":" + formatTime(seconds)}
-        </Text>
-      </View>
+      <ClockView remainSeconds={remainSeconds} />
       <View style={{ display: "flex", flex: 1 }}>
         <View style={{ display: "flex", flex: 1, justifyContent: "center" }}>
           <InputItem
---
+++ src/components/ClockView.tsx
@@ -0,0 +1,31 @@
+import { View, Text } from "react-native";
+export default function ClockView({
+  remainSeconds,
+}: {
+  remainSeconds: number;
+}) {
+  const formatTime = (time: number): string => {
+    time = Math.trunc(time);
+    return time >= 10 ? time + "" : "0" + time;
+  };
+  const minutes = remainSeconds / 60;
+  const seconds = remainSeconds % 60;
+  return (
+    <View
+      style={{
+        display: "flex",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 70,
+        }}
+      >
+        {formatTime(minutes) + ":" + formatTime(seconds)}
+      </Text>
+    </View>
+  );
+}
